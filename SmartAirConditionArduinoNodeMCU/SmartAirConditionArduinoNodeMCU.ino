4d5c2af9b5ed909a22b406acee0f8bff7b9d3776
Ran, Ido <ido.ran@hpe.com>
2017-07-11 23:56:15 +0300

    Found missing irsend.begin() to make IR LED work

--- SmartAirConditionArduinoNodeMCU/SmartAirConditionArduinoNodeMCU.ino
+++ SmartAirConditionArduinoNodeMCU/SmartAirConditionArduinoNodeMCU.ino
@@ -103,82 +103,78 @@ ESP8266WiFiMulti WiFiMulti;
 
 void setup() {
 
-    USE_SERIAL.begin(115200);
-   // USE_SERIAL.setDebugOutput(true);
+  irsend.begin();
+  
+  USE_SERIAL.begin(115200);
+ // USE_SERIAL.setDebugOutput(true);
 
-    USE_SERIAL.println();
-    USE_SERIAL.println();
-    USE_SERIAL.println();
+  USE_SERIAL.println();
+  USE_SERIAL.println();
+  USE_SERIAL.println();
 
-    for(uint8_t t = 4; t > 0; t--) {
-        USE_SERIAL.printf("[SETUP] WAIT %d...\n", t);
-        USE_SERIAL.flush();
-        delay(1000);
-    }
+  for(uint8_t t = 4; t > 0; t--) {
+      USE_SERIAL.printf("[SETUP] WAIT %d...\n", t);
+      USE_SERIAL.flush();
+      delay(1000);
+  }
 
-    WiFiMulti.addAP("Avital-Mamad", "avitallavie");
+  WiFiMulti.addAP("Avital-Mamad", "avitallavie");
 
 }
 
 void loop() {
 
-  for (int i = 0; i < 200000; i++) {
-    int cmd = i % 5;
-    sendIRCommand(cmd);
-    delay(1000);
+  // wait for WiFi connection
+  if((WiFiMulti.run() == WL_CONNECTED)) {
+
+      HTTPClient http;
+
+      USE_SERIAL.print("[HTTP] begin...\n");
+      // configure traged server and url
+      //http.begin("https://192.168.1.12/test.html", "7a 9c f4 db 40 d3 62 5a 6e 21 bc 5c cc 66 c8 3e a1 45 59 38"); //HTTPS
+      http.begin("http://ran-smart-home.appspot.com/api"); //HTTP
+
+      USE_SERIAL.print("[HTTP] GET...\n");
+      // start connection and send HTTP header
+      int httpCode = http.GET();
+
+      // httpCode will be negative on error
+      if(httpCode > 0) {
+          // HTTP header has been send and Server response header has been handled
+          USE_SERIAL.printf("[HTTP] GET... code: %d\n", httpCode);
+
+          // file found at server
+          if(httpCode == HTTP_CODE_OK) {
+              String payload = http.getString();
+              USE_SERIAL.println(payload);
+
+              char buffer[10];
+              payload.toCharArray(buffer, sizeof(buffer));
+              unsigned long etag = strtoul(buffer + 2, NULL, 10);
+              Serial.print( F("curr etag ") );
+              Serial.print(etag);
+              Serial.print( F("  last etag ") );
+              Serial.print(last_etag);
+              Serial.println();
+              if (etag == last_etag)
+              {
+                Serial.println( F("same etag, ignoring...") );
+              }
+              else
+              {
+                last_etag = etag;
+                EEPROMWritelong(LAST_ETAG_ADDRESS, last_etag);
+                unsigned int command_index = buffer[0] - '0';
+                sendIRCommand(command_index);          
+              }
+          }
+      } else {
+          USE_SERIAL.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+      }
+
+      http.end();
   }
-  
-    // wait for WiFi connection
-    if((WiFiMulti.run() == WL_CONNECTED)) {
 
-        HTTPClient http;
-
-        USE_SERIAL.print("[HTTP] begin...\n");
-        // configure traged server and url
-        //http.begin("https://192.168.1.12/test.html", "7a 9c f4 db 40 d3 62 5a 6e 21 bc 5c cc 66 c8 3e a1 45 59 38"); //HTTPS
-        http.begin("http://ran-smart-home.appspot.com/api"); //HTTP
-
-        USE_SERIAL.print("[HTTP] GET...\n");
-        // start connection and send HTTP header
-        int httpCode = http.GET();
-
-        // httpCode will be negative on error
-        if(httpCode > 0) {
-            // HTTP header has been send and Server response header has been handled
-            USE_SERIAL.printf("[HTTP] GET... code: %d\n", httpCode);
-
-            // file found at server
-            if(httpCode == HTTP_CODE_OK) {
-                String payload = http.getString();
-                USE_SERIAL.println(payload);
-
-                char buffer[10];
-                payload.toCharArray(buffer, sizeof(buffer));
-                unsigned long etag = strtoul(buffer + 2, NULL, 10);
-                Serial.print( F("curr etag ") );
-                Serial.print(etag);
-                Serial.print( F("  last etag ") );
-                Serial.print(last_etag);
-                Serial.println();
-                if (etag == last_etag)
-                {
-                  Serial.println( F("same etag, ignoring...") );
-                }
-                else
-                {
-                  last_etag = etag;
-                  EEPROMWritelong(LAST_ETAG_ADDRESS, last_etag);
-                  unsigned int command_index = buffer[0] - '0';
-                  sendIRCommand(command_index);          
-                }
-            }
-        } else {
-            USE_SERIAL.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
-        }
-
-        http.end();
-    }
-
-    delay(10000);
+  delay(10000);
 }
 
